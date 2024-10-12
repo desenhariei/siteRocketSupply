11ecf4dfc0799ed0d0d59e0cadbc13e749e1aa75
desenhariei <119636399+desenhariei@users.noreply.github.com>
2024-10-12 20:52:43 -0300

    error

--- src/mvc/models/intersectionModel.js
+++
@@ -1,6 +0,0 @@
-class IntersectionModel{
-    constructor(){
-        this.section02 = 'section02'
-        this.section01Videos = 'section video'
-    }
-}
--- src/mvc/views/carrosselViews.js
+++
@@ -1,6 +0,0 @@
-class CarrosselView {
-    changeBG(id,value){
-        id.style.background = `${value}`
-        id.style.backgroundSize = '100% auto'
-    }
-}
